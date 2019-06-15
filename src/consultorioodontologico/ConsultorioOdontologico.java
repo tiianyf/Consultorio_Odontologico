a549e26d92eabc64f21b6826f5ca6886c87a55e4
01ch01 <claudio.h1902@gmail.com>
2019-06-15 19:35:06 -0300

    -> adição das classes "Material", "DProprietario", "DColaborador"; -> alteração nome da classe "Cliente" para "Paciente"; -> remoção do atributo "doenca" da classe "Paciente"; -> adição dos atributos "problemaCardiaco", "doencaCronica", "alcoolatra", "historicoDiabetes", "coagulacao" na classe "Paciente"; -> alteração da cabeçalho -> classe *Dentista* é abstrata; -> alteração nome do atributo "tipo" (da classe "Procedimento") para "nome"; -> etc

--- src/consultorioodontologico/ConsultorioOdontologico.java
+++ src/consultorioodontologico/ConsultorioOdontologico.java
@@ -1,11 +1,21 @@
 package consultorioodontologico;
 
+import view.FrRequisicao;
+
 /**
+ * Sistema para um consultório odontológico com cadastro de funcionários,
+ * dentistas, procedimentos, controle de caixa e controle de marcação de
+ * procedimento. Roteiro obrigatório da disciplina de Orientação a Objetos do 3º
+ * Período.
  *
- * @author cfreitas <https://github.com/tiianyf> e Cláudio Henrique <https://github.com/01ch01>
+ * @author cfreitas <https://github.com/tiianyf> e Cláudio Henrique
+ * <https://github.com/01ch01>
  */
 public class ConsultorioOdontologico {
+
     public static void main(String[] args) {
+        FrRequisicao view = new FrRequisicao();
+        view.setEnabled(true);
     }
-    
+
 }
